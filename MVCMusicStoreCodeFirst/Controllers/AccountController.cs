56df463295b5bddacfb53dbfd9e42aabf85b041f
Connor Hansen <hanse174@ad.nmc.edu>
2019-04-29 19:29:12 -0400

    adding action results for Register

---
+++ MVCMusicStoreCodeFirst/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreCodeFirst.Models;
+
+
+
+namespace MVCMusicStoreCodeFirst.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AccountDBContext db = new AccountDBContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.firstName + " " + account.lastName + " successfully registered.";
+            }
+            return View();
+        }
+    }
+}
